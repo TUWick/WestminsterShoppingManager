7be4118f08694ba540d43b4509b1a3b7b232346a
Tenuri <tenuriw2002@gmail.com>
2023-12-17 19:41:39 +0530

    edits code

--- Clothing.java
+++ Clothing.java
@@ -2,6 +2,9 @@ public class Clothing extends Product {
     private int size;
     private String colour;
 
+    public Clothing(){
+    }
+
     public Clothing(String productID, String productName, int noOfAvailableItems, int price, int size, String colour) {
         super(productID, productName, noOfAvailableItems, price);
         this.size=size;
