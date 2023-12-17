482f704dcb6b8814d82c03307a6538ff2ff5798a
Tenuri <tenuriw2002@gmail.com>
2023-12-17 19:41:22 +0530

    edits code

--- Electronics.java
+++ Electronics.java
@@ -2,7 +2,11 @@ public class Electronics extends Product {
     private String brand;
     private int warrantyPeriod;
 
-    public Electronics(String productID, String productName, int noOfAvailableItems, int price, private String brand, private int warrantyPeriod){
+    public Electronics(){
+    }
+
+    
+    public Electronics(String productID, String productName, int noOfAvailableItems, int price, String brand, int warrantyPeriod){
         super(productID, productName, noOfAvailableItems, price);
         this.brand=brand;
         this.warrantyPeriod=warrantyPeriod;
--- Product.java
+++ Product.java
@@ -1,14 +1,44 @@
-abstract public class Product {
-    private String productID=null;
-    private String productName=null;
+import java.io.Serializable;
+
+abstract public class Product implements Serializable{
+    private String productID;
+    private String productName;
     private int noOfAvailableItems;
     private int price;
 
-    public Product(String productID,String productName,int noOfAvailableItems,int price){
+    public Products(){
+    }
+
+    public String Product(String productID,String productName,int noOfAvailableItems,int price){
         this.productID=productID;
         this.productName=productName;
         this.noOfAvailableItems=noOfAvailableItems;
         this.price=price;
+
+        public String getProductID(){
+            return productID;
+        }
+        public void setProductID(productID){
+            this.productID=productID;
+        }
+        public String getProductName(){
+            return productName;
+        }
+        public void setProductName(productName){
+            this.productName=productName;
+        }
+        public String getNoOfAvailableItems(){
+            return noOfAvailableItems;
+        }
+        public void setNoOfAvailableItems(noOfAvailableItems){
+            this.noOfAvailableItems=noOfAvailableItems;
+        }
+        public String getPrice(){
+            return price;
+        }
+        public void setPrice(price){
+            this.price=price;
+        }
     }
 
 
