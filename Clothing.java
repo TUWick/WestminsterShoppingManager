bb73f2cbb37feef1feb65289e1b0b571a1e8a617
Tenuri <tenuriw2002@gmail.com>
2023-12-17 18:25:18 +0530

    edits code

--- Clothing.java
+++ Clothing.java
@@ -1,3 +1,11 @@
-public class Clothing {
+public class Clothing extends Product {
+    private int size;
+    private String colour;
+
+    public Clothing(String productID, String productName, int noOfAvailableItems, int price, int size, String colour) {
+        super(productID, productName, noOfAvailableItems, price);
+        this.size=size;
+        this.colour=colour;
+    }
     
 }
--- Electronics.java
+++ Electronics.java
@@ -1,3 +1,12 @@
-public class Electronics {
+public class Electronics extends Product {
+    private String brand;
+    private int warrantyPeriod;
+
+    public Electronics(String productID, String productName, int noOfAvailableItems, int price, private String brand, private int warrantyPeriod){
+        super(productID, productName, noOfAvailableItems, price);
+        this.brand=brand;
+        this.warrantyPeriod=warrantyPeriod;
+
+    }
     
 }
