b818c13361a3d410d2e54329a049a268b208ea3f
Tenuri <tenuriw2002@gmail.com>
2023-12-17 18:17:57 +0530

    edits Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,16 @@
-public class Product {
-    
+abstract public class Product {
+    private String productID=null;
+    private String productName=null;
+    private int noOfAvailableItems;
+    private int price;
+
+    public Product(String productID,String productName,int noOfAvailableItems,int price){
+        this.productID=productID;
+        this.productName=productName;
+        this.noOfAvailableItems=noOfAvailableItems;
+        this.price=price;
+    }
+
+
+
 }
